17ced06cd7974cfca183d8a3f2352c49b6a17b1e
akai-hana <akaihanatsundeee@proton.me>
2026-02-06 16:57:57 +0100

    automated sync

--- src/bar/bar.zig
+++ src/bar/bar.zig
@@ -18,7 +18,7 @@ const workspaces             = @import("workspaces");
     const status_segment     = @import("status");
 
 // TODO: adjust workspace width based off of monitor DPI
-pub const WORKSPACE_WIDTH: u16 = 40;
+pub const WORKSPACE_WIDTH: u16 = 60;
 
 const State = struct {
     window: u32,
