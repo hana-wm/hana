c97f3d2a4b59aa45cbd86a352cafeb8f7b942069
akai-hana <akaihanatsundeee@proton.me>
2026-08-08 03:23:17 +0200

    chore(dev): tune automated sync loop script

--- dev/scripts/loop.sh
+++ dev/scripts/loop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-max=300
+max=250
+
 while true; do
-    yes y | git-rewrite-commits --provider ollama --model hf.co/noctrex/Qwopus3.5-9B-Coder-MTP \
+    yes y | git-rewrite-commits --provider ollama --model hf.co/KevinJK51/Qwen3.6-12B-IQ-Ultra-Heretic-Uncensored-Thinking-V2-Hightop-GGUF:IQ4_NL \
         --template "feat\(scope\): message" \
         --max-commits $max
 
     git push --force-with-lease
-    max=$((max + 10))
+    max=$((max + 1))
 done
